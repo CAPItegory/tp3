499cfcf7c713819ff2e5e5375a31f8169464d5e2
Axelle MAZUY <axelle.mazuy@gmail.com>
2025-01-10 12:28:48 +0100

    fix: responsive css for the size of content

--- shop-client/src/components/NavBar.tsx
+++ shop-client/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import SwitchLanguage from "./SwitchLanguage";
+import React from "react";
 
 const navItems = [
     { label: 'Boutiques', path: '/' },
@@ -10,26 +11,44 @@ const navItems = [
 
 const NavBar = () => {
     const navigate = useNavigate();
-
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
     return (
         <div>
-            <AppBar component="nav">
-                <Toolbar className="header">
-                    <Typography variant="h6" onClick={() => navigate('/')} sx={{ cursor: 'pointer' }}>
-                        Gestion de boutiques
-                    </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box>
-                        {navItems.map((item) => (
-                            <Button key={item.label} sx={{ color: '#fff' }} onClick={() => navigate(item.path)}>
-                                {item.label}
-                            </Button>
-                        ))}
-                    </Box>
-                    <Box>
-                        <SwitchLanguage />
-                    </Box>
-                </Toolbar>
+            <AppBar component="nav" position="static">
+                <Container maxWidth="xl">
+                    <Toolbar className="header">
+                        <Typography variant="h6" onClick={() => navigate('/')} sx={{ cursor: 'pointer' }}>
+                            Gestion de boutiques
+                        </Typography>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Box>
+                            {navItems.map((item) => (
+                                <Button key={item.label} sx={{ color: '#fff' }} onClick={() => navigate(item.path)}>
+                                    {item.label}
+                                </Button>
+                            ))}
+                        </Box>
+                        <Box>
+                            <SwitchLanguage />
+                        </Box>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </div>
     );
